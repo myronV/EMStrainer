d7cdb64a8c37812ee486f36e4b8e79c3caf724cf
Myron Voitovych <traftar4ik@gmail.com>
2018-05-02 16:19:10 +0300

    finish

---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=480">
+	<title>EMS-Trainer</title>
+	<meta name="description" content="EMS-Trainer - быстрая накачка мышц. EMS-Trainer - новинка 2018"/>
+	<meta name="keywords" content="EMS-Trainer, емс трейнер, пояс для мышц"/>
+	<link rel="icon" type="image/x-icon" href="/img/favicon.ico.png">
+	<link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico.png">
+	<!-- CSS -->
+	<link type="text/css" rel="stylesheet" href="css-familyOpenSanssubsetlatincyrillic.html"/>
+	<link type="text/css" rel="stylesheet" href="/css/style.css-2.css"/>
+	<!-- JS -->
+
+	<script type="text/javascript" src="/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/timer.js"></script>
+	<script type="text/javascript" src="/js/script.js-5.js"></script>
+
+
+	<link rel="stylesheet" type="text/css" href="/css/roboto.css">
+	<script src="/js/jquery.js" type="text/javascript"></script>
+	<script src="/js/plugins.js" type="text/javascript"></script>
+	<script src="/js/detect.js" type="text/javascript"></script>
+
+</head>
+<body>
+<div class=" ">
+	<div class="container">
+		<center>
+			<div class="discount1">-53%</div>
+			<div class="price" style="position: absolute; font-family: 'Open Sans';">
+				<div class="new">
+					<span class="price_land_s1">499</span> <span class="price_land_curr">грн.</span>
+				</div>
+				<div class="old">
+					<span class="price_land_s2">1070</span> <span class="price_land_curr">грн.</span>
+				</div>
+			</div>
+			<img class="pb-20" src="/img/1.jpg" alt="EMS-Trainer">
+		</center>
+		<a id="a1" href="#form" class="order">
+			<div></div>
+		</a>
+	</div>
+	<div class="container pt-40">
+		<center>
+			<img src="/img/2.jpg" alt="EMS-Trainer">
+		</center>
+		<p class="info">EMS-Trainer - уникальный тренажер для тренировки мышц пресса и спины, снятия боли в
+			миостимуляторничной области. Имеет 3 пары электродных накладок, что позволяет одновременно тренировать все
+			группы мышц брюшного пресса, талии, а в положении сзади - мышцы спины. Удобно, легко, эффективно - просто
+			наденьте миостимулятор, выберите нагрузку и Вам обеспечена превосходная тренировка всех труднодоступных мышц
+			живота, талии и спины.</p>
+	</div>
+	<div class="container pt-30">
+		<center>
+			<img src="/img/3.jpg" alt="EMS-Trainer">
+		</center>
+	</div>
+	<div class="container pt-70">
+		<center>
+			<img src="/img/4.jpg" alt="EMS-Trainer">
+		</center>
+	</div>
+	<div class="container pt-60">
+		<div class="how">
+			<p class="info two">Продукт является универсальным средством как для прокачки мышц, так и для похудения.
+				Кроме того, EMS-Trainer незаменим для тех, кто хочет навсегда избавиться от болей в миостимуляторнице.
+				Принцип работы миостимулятора EMS-Trainer весьма прост: миостимулятор посылает сигнал, который
+				стимулирует нервы, отвечающие за работу мышц.</p>
+			<a id="a2" href="#form" class="order two">
+				<div></div>
+			</a>
+		</div>
+	</div>
+
+<!--	<section class="promoBlock" id="promoBlock">-->
+<!--		<div class="container">-->
+<!--			<div class="bgPromo">-->
+<!--				<div class="left">-->
+<!--					<h2>Убойная акция от <span>EMS Trainer</span>!</h2>-->
+<!--					<div class="infoGift">-->
+<!--						<img src="/img/phone1.png" alt="">-->
+<!--						<h3>Дарим <span>iPhone X</span></h3>-->
+<!--						<p>Оформите заказ на покупку миостимулятора EMS Trainer <br><b>до 8-го июля</b> и участвуйте в-->
+<!--							розыгрыше iPhone X-->
+<!--						</p>-->
+<!--						<p class="info">Подробности в видео </p>-->
+<!--					</div>-->
+<!--				</div>-->
+<!--				<div class="right">-->
+<!--					<div class="video-container">-->
+<!--						 <div class="youtube" id="GeX2TT77LZU"></div>-->
+<!--						<iframe width="420" height="220" src="GeX2TT77LZU-rel=0&controls=0&showinfo=0" frameborder="0"-->
+<!--						        allowfullscreen></iframe>-->
+<!--					</div>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--	</section>-->
+
+	<div class="container">
+		<p class="header">Отзывы о продукте</p>
+		<div class="review">
+			<p class="name">Игорь Смирнов, 25 лет</p>
+			<center>
+				<img src="/img/photo-1.jpg" alt="EMS-Trainer">
+			</center>
+			<p class="pros">Сжигает жир</p>
+			<p class="txt">
+				Использую миостимулятор EMS-Trainer уже третью неделю и очень довольна результатами, талия стала намного
+				тоньше, и появился красивый рельеф, который я так хотела. Спасибо.
+			</p>
+		</div>
+		<div class="review">
+			<p class="name">Сергей, 34 года</p>
+			<center>
+				<img src="/img/photo-2.jpg" alt="EMS-Trainer">
+			</center>
+			<p class="pros">Это очень просто</p>
+			<p class="txt">Очень крутая вещь главное, что результат налицо, появился отличный рельефный пресс. Не буду
+				врать, хожу в качалку уже почти 2 года, поэтому основная мышечная масса там набрана. Но вот рельеф на
+				прессе — целиком заслуга миостимулятора, до него кубиков не было видно.
+			</p>
+		</div>
+	</div>
+	<div class="container pb-80">
+		<center>
+			<img src="/img/6.png" alt="EMS-Trainer">
+		</center>
+	</div>
+	<div class="container">
+		<div class="footer">
+			<div class="discount2">-53%</div>
+			<div class="price">
+				<div class="new">
+					<span class="price_land_s1">499</span> <span class="price_land_curr">грн.</span>
+				</div>
+				<div class="old">
+					<span class="price_land_s2">1070</span> <span class="price_land_curr">грн.</span>
+				</div>
+			</div>
+			<form action="zakaz.php" method="post"
+			      onsubmit="if(this.name.value==''){alert('Введите Ваше имя');return false}if(this.phone.value==''){alert('Введите Ваш номер телефона');return false}return true;"
+			      id="mobile_form" class="mobile_form orderformcdn">
+				<div class="input_div" id="form">
+					<input type="text" required placeholder="Ваше имя" name="name">
+					<input type="text" required placeholder="Ваш телефон" name="phone">
+
+					<button>ЗАКАЗАТЬ</button>
+				</div>
+			</form>
+			<article id="footer">
+				<section>
+
+					<p style="text-align: center">
+						<img src="/img/copy-w-320-5232.png" style="max-width:100%;"><br>
+						<a href="-page=politics.htm" target="_blank">Политика конфиденциальности</a>
+						<br>
+						<a href="-page=agreement.htm" target="_blank">Пользовательское соглашение</a>
+					</p>
+				</section>
+			</article>
+		</div>
+	</div>
+</div>
+<style>
+
+	.promoBlock {
+		display: none;
+	}
+
+	.promo {
+		background-image: linear-gradient(45deg, #9dc66b 5%, #4fa49a 30%, #87a0ef);
+		position: fixed;
+		width: 300px;
+		height: 112px;
+		margin: 0 auto;
+		bottom: 0;
+		z-index: 99;
+		left: 0;
+		right: 0;
+		border-radius: 10px 10px 0 0;
+		font-family: 'Roboto';
+	}
+
+	.promo:before {
+		content: "";
+		position: absolute;
+		width: 86px;
+		background: url(./img/phone2.png) no-repeat;
+		height: 121px;
+		top: -11px;
+		left: 5px;
+	}
+
+	.close {
+		position: absolute;
+		background: #5d687a;
+		width: 20px;
+		height: 20px;
+		text-align: center;
+		border-radius: 50%;
+		line-height: 19px;
+		cursor: pointer;
+		font-weight: bold;
+		color: #fff;
+		right: -5px;
+		top: -7px;
+		font-family: 'Roboto';
+		font-size: 18px;
+	}
+
+	.promo p {
+		display: inline-block;
+		color: #fff;
+		font-size: 16px;
+		padding: 10px 0 0 100px;
+		font-family: 'Roboto';
+		font-weight: normal;
+	}
+
+	.promo p span {
+		font-size: 24px;
+	}
+
+	.promo p span:after {
+		content: "";
+		background: url(./img/x.png) no-repeat;
+		width: 20px;
+		height: 23px;
+		position: absolute;
+	}
+
+	a.btnPromo {
+		color: #fff;
+		text-decoration: none;
+		width: 170px;
+		height: 35px;
+		text-align: center;
+		line-height: 35px;
+		border-radius: 20px;
+		background: #0b5889;
+		font-size: 17px;
+		margin: 5px 30px 0 0;
+		display: block;
+		float: right;
+		font-family: 'Roboto';
+	}
+
+	.bgPromo {
+		background: url(./img/bg320.jpg) no-repeat;
+		width: 100%;
+		height: auto;
+		padding: 20px;
+		margin-bottom: 40px;
+		clear: both;
+		overflow: hidden;
+		background-size: cover;
+	}
+
+	.bgPromo .video-container {
+		width: 420px;
+		height: 220px;
+	}
+
+	.bgPromo .left, .bgPromo .right {
+		float: none;
+		margin: 0 auto;
+		width: 420px;
+	}
+
+	.bgPromo h2 {
+		color: #fff;
+		margin-bottom: 20px;
+		padding-bottom: 0;
+		font-family: 'Roboto';
+		line-height: 1.2;
+		font-size: 24px;
+		text-align: center;
+	}
+
+	.bgPromo h2 span {
+		color: #ffc600;
+	}
+
+	.bgPromo .infoGift {
+		clear: both;
+		overflow: hidden;
+		color: #fff;
+	}
+
+	.infoGift img {
+		float: left;
+		margin-right: 20px;
+	}
+
+	.infoGift h3 {
+		margin: 10px;
+		font-size: 30px;
+		font-family: 'Roboto';
+		padding: 0;
+	}
+
+	.infoGift p {
+		font-family: 'Roboto';
+	}
+
+	.bgPromo p {
+		padding: 0;
+	}
+
+	.bgPromo p.info {
+		margin-top: 20px;
+		color: #1d73ca;
+		position: relative;
+	}
+
+	.bgPromo p.info:after {
+		content: "";
+		position: absolute;
+		width: 11px;
+		height: 5px;
+		background: url(./img/giftpromo.png) no-repeat;
+		top: 10px;
+		margin-left: 10px;
+	}
+</style>
+
+
+<div class="promo" id="modal" style="display:none;">
+	<div class="close" data-toggle="modal">&times;</div>
+	<p>Закажите EMS Trainer и выиграйте <span>iPhone</span></p>
+	<a href="#promoBlock" class="btnPromo">Узнать подробнее</a>
+</div>
+
+
+<script type="text/javascript">
+
+	//$(".promo").fadeIn(5000)
+
+	$(function () {
+		$('.close').click(function () {
+			$(".promo").hide(200);
+		});
+		$('.btnPromo').click(function () {
+			$(".promo").hide(200);
+		});
+
+	});
+</script>
+
+
+<style>
+
+	.bgPromo {
+		padding: 20px 0;
+	}
+</style>
+
+
+</body>
+</html>
+
+
---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('[name="country"]').on('change', function() {
+        var geoKey = $(this).find('option:selected').val();
+        var data = $jsonData.prices[geoKey];
+        var price = data.price;
+        var oldPrice = data.old_price;
+        var delivery_price = data.delivery_price;
+        var currency = data.currency
+        $("[value = "+geoKey+"]").attr("selected", true).siblings().attr('selected', false);
+
+        $('.price_land_s1').text(price);
+        $('.price_land_s2').text(oldPrice);
+        $('.price_land_curr').text(currency);
+    });
+
+
+    $('.order').click( function(){ 
+        var scroll_el = $(this).attr('href'); 
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+        }
+        return false;
+    });
+}); 
